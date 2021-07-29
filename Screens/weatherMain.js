91635922d924287949e5aaba6aa0a80eeefdac6e
will <william.jl.johnson@gmail.com>
2021-07-29 19:52:15 +0200

    feat: Flatlist rendering with data - GOOD COMMIT

--- Screens/weatherMain.js
+++ Screens/weatherMain.js
@@ -2,36 +2,45 @@ import { StatusBar } from 'expo-status-bar';
 import React, { useState, useEffect } from 'react';
 import { FlatList, StyleSheet, Text, View } from 'react-native';
 
-const lat = 58.7984;
-const lng = 17.8081;
-const params = 'waveHeight,airTemperature';
+const lat = 41.376700;
+const lng = 2.193972;
+const params = 'visibility';
+const start = '2021-07-29';
+const end = '2021-07-30';
+const API_URL = `https://api.stormglass.io/v2/weather/point?lat=${lat}&lng=${lng}&params=${params}&start=${start}&end=${end}`;
 
 export default function WeatherMain() {
+
+  const renderItem = ({ item }) => {
+    return <View>
+      <Text> {item.visibility.sg} </Text>
+    </View>
+  }
 
   const [marineWeather, setMarineWeather] = useState([]);
 
   useEffect(() => {
-    fetch( process.env.seaWeatherAPI, {
+
+    fetch( API_URL, {
       headers: {
-        'Authorization': process.env.APIKey
+        'Authorization': process.env.EXPO_APP_APIKEY
       }
-    })
-    .then((response) => response.json())
-    .then((jsonData) => { setMarineWeather(jsonData) });   
-  })
+    }
+  )
+    .then((response) => (response.json()))
+    .then((jsonData) => { setMarineWeather(jsonData.hours) })
+  },[])
+
   
   return (
   
-    <View style={styles.container}>
+    <View>
       <FlatList 
-        data = {marineWeather}
-        renderItem = { ({item}) => (
-          <Text>{item}</Text>
-        )}
-      />
+        data={ marineWeather }
+        keyExtractor={(item) => item.time}
+        renderItem={ renderItem }
+      /> 
     </View> 
-    
-
   );
 }
 
@@ -41,5 +50,6 @@ const styles = StyleSheet.create({
     backgroundColor: '#f1faee',
     alignItems: 'center',
     justifyContent: 'center',
+    color: 'black',
   },
 });
