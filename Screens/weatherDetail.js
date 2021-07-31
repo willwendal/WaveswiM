116670bb3520ba3b4df2c8a6251f2581c215f248
will <william.jl.johnson@gmail.com>
2021-07-31 18:22:42 +0200

    style: background added to weatherDetail

--- Screens/weatherDetail.js
+++ Screens/weatherDetail.js
@@ -2,6 +2,13 @@ import { StatusBar } from 'expo-status-bar';
 import React, { useState, useEffect } from 'react';
 import { FlatList, StyleSheet, Text, View, ImageBackground } from 'react-native';
 
+const lat = 41.376700;
+const lng = 2.193972;
+const params = 'airTemperature';
+const start = '2021-07-31';
+const end = '2021-08-01';
+const API_URL = `https://api.stormglass.io/v2/weather/point?lat=${lat}&lng=${lng}&params=${params}&start=${start}&end=${end}`;
+
 
 export default function WeatherDetail() {
 
