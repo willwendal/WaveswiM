a72db658f3e8b7c254de8216a1904d1592324c90
will <william.jl.johnson@gmail.com>
2021-07-31 15:08:44 +0200

    style: layout finished for weatherMain - GOOD COMMIT

--- Screens/weatherMain.js
+++ Screens/weatherMain.js
@@ -23,11 +23,10 @@ export default function WeatherMain() {
 
   const renderItem = ({ item }) => {
 
-  
     return <Text style={styles.weatherBar}> 
               {item.cloudCover.sg} 
             </Text>       
-}
+  }
 
   const [marineWeather, setMarineWeather] = useState([]);
 
@@ -35,7 +34,7 @@ export default function WeatherMain() {
 
     fetch( API_URL, {
       headers: {
-        'Authorization': .env,
+        'Authorization': process.env.APIKEY,
       }
     }
   )
@@ -49,42 +48,76 @@ export default function WeatherMain() {
   return (
   
     <View style={styles.container} >
-      <ImageBackground
+      {/* <ImageBackground
         source={require('../assets/waveswim_background.png')}
         style={{width: '100%', height: '100%'}}
-      >
+      > */}
         
           <Text style={styles.header}>Barcelona
             28°C
           </Text>
-        
-        <View>
+      <View style={styles.weatherBar}>
+        <View style={styles.weatherBarLeft}>
           {days.map((day) => {
-            return <Text>{day}</Text>
+          return <Text>
+                  {day}
+                </Text>
           })}
-          
         </View>
-      </ImageBackground>
+        <View>
+          <FlatList
+            style={styles.weatherBarRight} 
+            data={ marineWeather }
+            keyExtractor={(item) => item.time}
+            renderItem={ renderItem }
+          /> 
+        </View>
+      </View>
+      {/* </ImageBackground> */}
     </View> 
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
     display: 'flex',
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  backgroundImage: {
     flex: 1,
+    backgroundColor: 'black',
   },
   header: {
     padding: 100,
     textAlign: 'center',
   },
   weatherBar: {
-    textAlign: 'right',
-    padding: 20,
-  }
+    flexDirection: 'row',
+    display: 'flex',
+    flexWrap: 'wrap',
+    justifyContent: 'space-between',
+    backgroundColor: 'blue'
+  },
+  weatherBarLeft: {
+    padding: 5,
+    marginLeft: 10,
+    backgroundColor: 'green',
+    alignSelf: 'flex-start',
+  },
+  weatherBarRight: {
+    padding: 10,
+    alignSelf: 'flex-end',
+    marginRight: 20,
+    backgroundColor: 'red',
+  },
 });
+
+
+
+
+{/* <FlatList 
+data={ marineWeather }
+keyExtractor={(item) => item.time}
+renderItem={ renderItem }
+/> */}
+
+
+
+
